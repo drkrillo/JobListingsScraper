e3f622f608bd7d08d93053d13ce745b8ed9e588a
Juan Pasutti <juanpasutti@clear-tech.com>
2022-09-30 14:47:36 -0300

    defined fields

--- craiglist/items.py
+++ craiglist/items.py
@@ -1,14 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class CraiglistItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    # Define fields to be scraped
     date = scrapy.Field()
     link = scrapy.Field()
     text = scrapy.Field()
